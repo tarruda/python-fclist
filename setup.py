401813ccd208795d9ab1df24484650fc90b18834
Thiago de Arruda <tpadilha84@gmail.com>
2014-12-16 11:05:29 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ else:
     long_description = description
 
 setup(name='fclist',
-      version='1.0.0',
+      version='1.1.0',
       description=description,
       long_description=long_description,
       url='http://github.com/tarruda/python-fclist',
-      download_url='https://github.com/tarruda/python-fclist/archive/1.0.0.tar.gz',
+      download_url='https://github.com/tarruda/python-fclist/archive/1.1.0.tar.gz',
       author='Thiago de Arruda',
       author_email='tpadilha84@gmail.com',
       license='MIT',
