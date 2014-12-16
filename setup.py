1a92e3f8eaee95b271664d9fe2af86baac5ebf03
Thiago de Arruda <tpadilha84@gmail.com>
2014-12-16 11:04:40 -0300

    Add long description to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
+import os
 from setuptools import setup
+
+description="Python cffi bridge to fontconfig's FcFontList/FcFontMatch",
+if os.path.exists('README.md'):
+    long_description = open('README.md').read()
+else:
+    long_description = description
 
 setup(name='fclist',
       version='1.0.0',
-      description="Python cffi bridge to fontconfig's FcFontList/FcFontMatch",
+      description=description,
+      long_description=long_description,
       url='http://github.com/tarruda/python-fclist',
       download_url='https://github.com/tarruda/python-fclist/archive/1.0.0.tar.gz',
       author='Thiago de Arruda',
